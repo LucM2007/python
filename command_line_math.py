111cf3b71e45d996ec829100a2231c13f2a6b2e5
ihaveawhitevan <ps252564@edu.summacollege.nl>
2025-01-16 14:19:42 +0100

    added pemdas to calculator

--- command_line_math.py
+++ command_line_math.py
@@ -1,40 +1,65 @@
-number1 = input("what do you want to put in number 1? ")
-a = int(number1)
-number2 = input("what do you want to put in number 2? ")
-b = int(number2)
-
-
 while True:
-    operator = input("Enter wich operator you wanne use: ").capitalize()
-
-    match operator:
-        case "Times":
-            if(a==0 or b==0):
-                print("You can't multiply by 0")
-                if(a==0):
-                    a = int(input("Enter a new number for number 1: "))
-                else:
-                    b = int(input("Enter a new number for number 2: "))
-            print( a*b)  
-            break
-        case "Plus":
-            print(a+b)
-            break
-        case "Minus":
-            print(a-b)
-            break
-        case "Divide":
-            if(a==0 or b==0):
-                print("You can't multiply by 0")
-                if(a==0):
-                    a = int(input("Enter a new number for number 1: "))
-                else:
-                    b = int(input("Enter a new number for number 2: "))
-            
-            print(a/b)
-            break
-        case "Power":
-            print(a**b)
-            break
-        case _:
-            print("Invalid operator. Please try again. you can chose between Times, Plus, Minus, Divide and Power")
+    play = input("Do you want to play? (yes/no): ").strip().lower()
+    if play != 'yes':
+        exit()
+    how_manny_numbers = int(input("how many numbers do you want to use? "))
+    question = []
+    operators = ["power", "times", "divide", "plus", "minus"]
+    if how_manny_numbers < 1:
+        print("you need to enter a number greater than 0")
+    else:
+        for i in range(how_manny_numbers):
+            number = input(f'what do you want to make number {i + 1}? ')
+            if number.isdigit():
+                question.append(int(number))
+            else:
+                print("you need to enter a number")
+                break
+            if i != how_manny_numbers - 1:
+                operator = input("what operator do you want to use: ")
+                question.append(operator)
+        for j in range(len(operators)):
+            while operators[j] in question:
+                for k in range(len(question)):
+                    if question[k] == operators[j]:
+                        if operators[j] == "power":
+                            result = question[k-1]**question[k+1]
+                            print(f'{question[k-1]}^{question[k+1]} = {result}')
+                        elif operators[j] == "times":
+                            if question[k-1] == 0 or question[k+1] ==0:
+                                if question[k-1] == 0:
+                                    NewNumber = input("You can't multiply by 0, please enter a new number: ")
+                                    if NewNumber.isdigit():
+                                        question[k-1] = int(NewNumber)
+                                if question[k+1] == 0:
+                                    NewNumber = input("You can't multiply by 0, please enter a new number: ")
+                                    if NewNumber.isdigit():
+                                        question[k+1] = int(NewNumber)
+                            result = question[k-1] * question[k+1]
+                            print(f'{question[k-1]}*{question[k+1]} = {result}')
+                        elif operators[j] == "divide":
+                            if question[k-1] == 0 or question[k+1] == 0:
+                                if question[k-1] == 0:
+                                    NewNumber = input("You can't divide by 0, please enter a new number: ")
+                                    if NewNumber.isdigit():
+                                        question[k-1] = int(NewNumber)
+                                if question[k+1] == 0:
+                                    NewNumber = input("You can't divide by 0, please enter a new number: ")
+                                    if NewNumber.isdigit():
+                                        question[k+1] = int(NewNumber)
+                            result = question[k-1] / question[k+1]
+                            print(f'{question[k-1]}/{question[k+1]} = {result}')
+                        elif operators[j] == "plus":
+                            result = question[k-1] + question[k+1]
+                            print(f'{question[k-1]}+{question[k+1]} = {result}')
+                        elif operators[j] == "minus":
+                            result = question[k-1] - question[k+1]
+                            print(f'{question[k-1]}-{question[k+1]} = {result}')
+                        else:
+                            print("Something went wrong, please try again and use real operators (power, times, divide, plus, minus)")
+                            break
+                        question.pop(k+1)
+                        question.pop(k)
+                        question.pop(k-1)                        
+                        question.insert(k-1, result)
+                        break
